bdd52c657e03b0c7df1c77cfd92b51f37ad202f5
Mohammad-Awayssa <mohammadbassamr@gmail.com>
2026-08-02 20:42:05 +0300

    create fileserver and add serving image:C1L6&7

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+import express from "express";
+
+const app = express();
+const port = 8080;
+
+app.use(express.static("."));
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
