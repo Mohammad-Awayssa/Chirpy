ae453677b258eaac4b778c2aaf78d5ffd82cbec2
Mohammad-Awayssa <mohammadbassamr@gmail.com>
2026-08-02 22:59:24 +0300

    add middlerware function that store logs:C2L1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express,{ Request, Response } from "express";
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./api/middleware.js";
 
 const app = express();
 const port = 8080;
 
+
+app.use(middlewareLogResponses);
 app.use("/app",express.static("./src/app"));
 
 app.get("/healthz", handlerReadiness);
