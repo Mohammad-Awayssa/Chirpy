4c2d0863ec49055f4758dfaed67052d348a90f55
Mohammad-Awayssa <mohammadbassamr@gmail.com>
2026-08-02 22:41:09 +0300

    costum handler for readiness endpoint:C1L10

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import express from "express";
+import express,{ Request, Response } from "express";
+import { handlerReadiness } from "./api/readiness.js";
 
 const app = express();
 const port = 8080;
 
-app.use(express.static("."));
+app.use("/app",express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
