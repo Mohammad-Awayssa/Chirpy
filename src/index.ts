478e8c6e86eed63f799898796a836ffbd076bc8d
Mohammad-Awayssa <mohammadbassamr@gmail.com>
2026-08-04 00:11:06 +0300

    update metrics page:C3L3

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,9 @@ app.use(middlewareLogResponses);
 
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/api/metrics", handlerMetrics);
-app.get("/api/reset", handlerReset);
-app.get("/api/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerMetrics);
+app.get("/admin/reset", handlerReset);
+app.get("/admin/healthz", handlerReadiness);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
